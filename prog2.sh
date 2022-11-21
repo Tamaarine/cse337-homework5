f5c4277ba8b48026dff51262a6f295bfa86c2727
Tamaarine <irebootplaygt@gmail.com>
2022-11-21 18:03:07 -0500

    Defined: program 2's script is done

--- prog2.sh
+++ prog2.sh
@@ -6,15 +6,36 @@ FILE_NOT_FOUND_MSG="file not found"
 MORE_ARGS_MSG="Exactly 2 arguments required"
 
 # Checking args
-if [ $# -gt 2 ] || [ $# -eq 1 ]; then
-    echo $MORE_ARGS_MSG
-    exit 0
-elif [ $# -eq 0 ]; then
+if [ $# -ne 2 ]; then
     echo $WRONG_ARGS_MSG
     exit 0
 fi
 
 if [ ! -f $1 ]; then
-    echo $FILE_NOT_FOUND_MSG
+    echo "`basename $1` $FILE_NOT_FOUND_MSG"
     exit 0;
 fi
+
+mkdir -p awk
+cat << 'HEREDOC' > awk/temp1
+BEGIN {
+    largest_nf=0
+}
+{
+    if (NF > largest_nf)
+    {
+        largest_nf=NF
+    }
+    for(i=1;i<=NF;i++) {
+        sum[i]+=$i
+    }
+}
+END {
+    for(i=1;i<=largest_nf;i++) {
+        print "Col " i " : " sum[i]
+    }
+}
+HEREDOC
+
+awk -F ",|:|;" -f awk/temp1 $1 > $2
+rm -rf awk
