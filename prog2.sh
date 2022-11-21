c680869583519122bd008feca42598efd4050889
Tamaarine <irebootplaygt@gmail.com>
2022-11-21 16:54:17 -0500

    Defined: argument checking for second script

--- prog2.sh
+++ prog2.sh
@@ -3,5 +3,18 @@
 # Constants; add more if necessary
 WRONG_ARGS_MSG="data file or output file missing"
 FILE_NOT_FOUND_MSG="file not found"
+MORE_ARGS_MSG="Exactly 2 arguments required"
 
-# FILL ME
+# Checking args
+if [ $# -gt 2 ] || [ $# -eq 1 ]; then
+    echo $MORE_ARGS_MSG
+    exit 0
+elif [ $# -eq 0 ]; then
+    echo $WRONG_ARGS_MSG
+    exit 0
+fi
+
+if [ ! -f $1 ]; then
+    echo $FILE_NOT_FOUND_MSG
+    exit 0;
+fi
